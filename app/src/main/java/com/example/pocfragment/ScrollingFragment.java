f6a185dec076e377b50e136e089293483647634e
shafri amir <shafri.amir@tiongnam.local>
2020-10-20 11:14:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pocfragment/ScrollingFragment.java
@@ -0,0 +1,23 @@
+package com.example.pocfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ScrollingFragment extends Fragment {
+    public ScrollingFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_scrolling, container, false);
+    }
+}
